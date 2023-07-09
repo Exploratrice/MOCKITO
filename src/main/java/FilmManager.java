87cfd4576e92085b8cb9c6ac54668c08d224ccf7
Explora <exploratrice2019@mail.ru>
2023-07-09 15:16:18 +0300

    initial commit

---
+++ src/main/java/FilmManager.java
@@ -0,0 +1,16 @@
+public class FilmManager {
+
+    ...
+            ??? resultLength;
+  if ??? {
+        resultLength = ???
+    } else {
+        resultLength = ???
+    }
+  ??? result = new ???[resultLength];
+  for (???) {
+        // заполняем result из массива, что лежит в поле
+        // в обратном порядке
+    }
+...
+}
---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,15 @@
+public class FilmManagerTest {
+    ...
+            ??? resultLength;
+  if ??? {
+        resultLength = ???
+    } else {
+        resultLength = ???
+    }
+  ??? result = new ???[resultLength];
+  for (???) {
+        // заполняем result из массива, что лежит в поле
+        // в обратном порядке
+    }
+...
+}
